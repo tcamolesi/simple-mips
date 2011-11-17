f27f98278030e39462416c84f934145b725a0a98
Tiago Camolesi <tiagocamolesi@gmail.com>
2011-11-17 00:47:30 -0200

    Refactored registers and register bank.

--- src/mips/rtl/core_pack.vhd
+++ src/mips/rtl/core_pack.vhd
@@ -5,4 +5,18 @@ package core_pack is
 
   subtype dw_t is std_logic_vector(31 downto 0);
 
+  component reg_g is
+    generic (
+      nbits_g : natural := dw_t'length
+    );
+    port (
+      d_i     : in std_logic_vector(nbits_g - 1 downto 0);
+      ck_i    : in std_logic;
+      rst_i   : in std_logic;
+      wr_en_i : in std_logic;
+
+      q_o     : out std_logic_vector(nbits_g - 1 downto 0)
+  );
+  end component;
+
 end package;
